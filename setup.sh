23d30dfd6a5deed1d028ae92b0bef21d99db2e25
TeriForey <teri.forey@gmail.com>
2018-05-03 10:53:55 +0100

    Removed requirements and setup.sh files that were specific to running on ALICE

--- setup.sh
+++
@@ -1,9 +0,0 @@
-module load python/gcc/27
-module load gdal
-module load geos
-module load spatialindex/1.8.5
-. ./venv.geo/bin/activate
-export LD_LIBRARY_PATH="/cm/shared/apps/geos/3.6.1/lib:"$LD_LIBRARY_PATH
-export GEOS_DIR="/cm/shared/apps/geos/3.6.1/"
-export SPATIALINDEX_C_LIBRARY="/cm/shared/apps/spatialindex/1.8.5/lib/libspatialindex_c.so"
-export PATH=$PATH:.
