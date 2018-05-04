1d5db27aa402ff568ba6046a9f812200b932557a
TeriForey <teri.forey@gmail.com>
2018-05-04 11:49:59 +0100

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyNAMEplot',
+      version='0.1.0',
+      description='Plots output of the NAME model',
+      url='https://github.com/TeriForey/pyNAMEplot',
+      author='Teri Forey',
+      author_email='trf5@le.ac.uk',
+      license='AL',
+      packages=['pynameplot'],
+      )
