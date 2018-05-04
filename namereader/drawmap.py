0b1e5b04b4f2cebd45daa2bef0ea427efd2790ba
TeriForey <teri.forey@gmail.com>
2018-05-04 09:58:30 +0100

    Moved drawMap function into the library and created new file to run from command line

---
+++ namereader/drawmap.py
@@ -0,0 +1,122 @@
+import os
+import namemap
+
+
+def drawMap(n, column, projection=False, lon_bounds=(), lat_bounds=(), lon_axis=[], lat_axis=[],
+            scale=(), autoscale=True, caption=False, solid=False, color1=False, colormap=False, station=False,
+            outdir="", outfile=False):
+    """
+    Function will draw a footprint map, most values will not need to be set as defaults are okay.
+    :param n: Name obj
+    :param column: Column obj
+    :param projection: string, 'cyl' by default
+    :param lon_bounds: tuple longitudinal boundary
+    :param lat_bounds: tuple latitude boundary
+    :param lon_axis: tuple longitude axis
+    :param lat_axis: tuple latitude axis
+    :param scale: tuple
+    :param autoscale: bool
+    :param caption: string
+    :param solid: bool
+    :param color1: string
+    :param colormap: string
+    :param station: tuple
+    :param outdir: string
+    :param outfile: string
+    :return:
+    """
+    # Create Map object from NAME data
+    m = namemap.Map(n, column=column)
+
+    # Set projection if defined, otherwise cylindrical
+    if projection:
+        m.setProjection(projection)
+
+    # Set map bounds from config file, otherwise scale by grid file
+    if lon_bounds and lat_bounds:
+        m.setBounds(lon_bounds, lat_bounds)
+    else:
+        m.setBounds(n.lon_bounds, n.lat_bounds)
+
+    # Set map axes from config file, else scale by grid file
+    if lon_axis and lat_axis:
+        lon = [float(i) for i in lon_axis]
+        lat = [float(i) for i in lat_axis]
+        m.setAxes(lon, lat)
+    else:
+        m.setAxes(n.lon_grid, n.lat_grid)
+
+    # Set scale if defined, otherwise standard scale
+    if scale:
+        m.setFixedScale(conc=scale)
+    elif autoscale:
+        m.setAutoScale(column)
+
+    # Set up data grid
+    if caption:
+        m.drawBase(caption, fontsize=8)
+    else:
+        m.drawBase(m.caption, fontsize=8)
+
+    # Check for solid colouring flag
+    if solid:
+        m.solid = True
+        if color1:
+            m.drawSolid(column, color=color1)
+        else:
+            m.drawSolid(column)
+    elif colormap:  # Plot using colormap
+        m.setColormap(colormap)
+        m.drawMesh(column)
+    else:
+        m.setColormap()
+        m.drawMesh(column)
+
+    # Add station marker if defined
+    if station:
+        (station_lon, station_lat) = station
+        m.addMarker(station_lon, station_lat)
+
+    # If output directory does not exist, create it
+    if len(outdir) > 0:
+        if not os.path.exists(outdir):
+            os.makedirs(outdir)
+        m.outdir = outdir
+
+    # Save output to disk
+    if outfile:
+        m.saveFile(filename=outfile)
+    else:
+        m.saveFile()
+
+
+def draw_shape_map(n, column, shapelist, shapelines=True, shapecolors=True):
+
+    m = drawMap(n, column)
+
+    files = []
+    colors = []
+    with open(shapelist, 'r') as f:
+        for line in f:
+            if "," in line:
+                (filename, colorname) = line.split(",", 1)
+                filename = filename.strip()
+                colorname = colorname.strip()
+
+                files.append(filename)
+                colors.append(colorname)
+
+    # Load zones into map object
+    m.zoneLoad(files)
+
+    #        print m.patches # DEBUG
+
+    # Draw lines at zone boundaries
+    if shapelines:
+        print 'Plotting zone lines...'
+        m.zoneLines()
+
+    # Draw solid colours for zones
+    if shapecolors:
+        print 'Plotting zone colours...'
+        m.zoneColour(colors)
---
+++ plot_footprint.py
@@ -0,0 +1,119 @@
+#! /usr/bin/env python
+
+import os
+import argparse
+import tempfile
+import shutil
+import calendar
+from datetime import datetime
+
+from namereader import drawmap
+from namereader import name
+from namereader import namesum
+
+
+class TemporaryDirectory(object):
+    """Context manager for tempfile.mkdtemp() so it's usable with "with" statement."""
+    def __enter__(self):
+        self.name = tempfile.mkdtemp()
+        return self.name
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        shutil.rmtree(self.name)
+
+
+parser = argparse.ArgumentParser(prog='plot_footprint', formatter_class=argparse.RawDescriptionHelpFormatter,
+                                 description='Plot NAME concentration files on world map')
+parser.add_argument('-i', '--infiles', nargs='+', required=True, help="NAME output files to plot")
+parser.add_argument('-o', '--outputdir', nargs='?', required=True, help="Plot output directory")
+parser.add_argument('-t', '--time', nargs='?', help='Specific datetime to plot (YYYY-MM-DD HH:MM UTC)')
+parser.add_argument('-d', '--day', nargs='?', help='Plot summary of this day')
+parser.add_argument('-w', '--week', nargs='?', help='Plot summary of this week')
+parser.add_argument('-m', '--month', nargs='?', help='Plot summary of this month')
+parser.add_argument('-y', '--year', nargs='?', help='Plot summary of this year')
+parser.add_argument('-a', '--all', action='store_true', default=False, help='Plot summary of all files')
+
+args = parser.parse_args()
+
+print args
+
+if len(args.infiles) == 1:
+    n = name.Name(args.infiles[0])
+    if args.time:
+        # draw map for single timestamp
+        column = args.time
+        n.column = column
+        drawmap.drawMap(n, column, outdir=args.outputdir)
+    else:
+        # draw maps for all timestamps in file
+        for column in n.timestamps:
+            print n.timestamps
+            n.column = column
+            drawmap.drawMap(n, column, outdir=args.outputdir)
+
+else:
+    # Copy files to a temporary directory
+    with TemporaryDirectory() as tmpdir:
+        for f in args.infiles:
+            shutil.copy(f, tmpdir)
+
+        s = namesum.Sum(tmpdir)
+        column = 'total'
+
+        if args.day:
+            # draw summed map for day
+            s.sumDay(args.day)
+
+            caption = "{} {} {} {}: {}{}{} day sum".format(s.runname, s.averaging, s.altitude, s.direction, s.year,
+                                                           s.month, s.day)
+            outfile = "{}_{}{}{}_daily.png".format(s.runname, s.year, s.month, s.day)
+
+            drawmap.drawMap(s, column, caption=caption, outfile=outfile, outdir=args.outputdir)
+
+        elif args.week:
+            # draw summed map for week
+            s.sumWeek(args.week)
+
+            caption = "{} {} {} {}: {} week {} sum".format(s.runname, s.averaging, s.altitude, s.direction, s.year,
+                                                           args.week)
+            outfile = "{}_{}{}_weekly.png".format(s.runname, s.year, args.week.zfill(2))
+
+            drawmap.drawMap(s, column, caption=caption, outfile=outfile, outdir=args.outputdir)
+
+        elif args.month:
+            # draw summed map for month
+            s.sumMonth(args.month)
+
+            caption = "{} {} {} {}: {} {} sum".format(s.runname, s.averaging, s.altitude, s.direction, s.year,
+                                                      calendar.month_name[int(args.month)])
+            outfile = "{}_{}{}_monthly.png".format(s.runname, s.year, args.month.zfill(2))
+            drawmap.drawMap(s, column, caption=caption, outfile=outfile, outdir=args.outputdir)
+
+        elif args.year:
+            # draw summed map for year
+            s.sumYear(args.year)
+
+            caption = "{} {} {} {}: {} year sum".format(s.runname, s.averaging, s.altitude, s.direction, args.year)
+            outfile = "{}_{}_yearly.png".format(s.runname, args.year)
+
+            drawmap.drawMap(s, column, outdir=args.outputdir)
+
+        elif args.all:
+            # draw summed map for entire directory
+            s.sumAll()
+
+            caption = "{} {} {} {}: Summed".format(s.runname, s.averaging, s.altitude, s.direction)
+            outfile = "{}_summed_all.png".format(s.runname)
+            drawmap.drawMap(s, column, outdir=args.outputdir)
+
+        else:
+            # draw maps for all timestamps and files in directory
+            allfiles = sorted(s.fs.getAll())
+
+            for f in allfiles:
+                n = name.Name(f)
+                for column in n.timestamps:
+                    n.column = column
+                    drawmap.drawMap(n, column, outdir=args.outputdir)
+
+    # End with so tempdir is deleted
